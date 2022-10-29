32ca0bb5df08c586cd07ac1c9d3e45a8e449f553
Vanja Sretenović <31417903+vanja032@users.noreply.github.com>
2022-10-29 02:28:40 +0200

    Update LogTrimmer.py

--- LogTrimmer.py
+++ LogTrimmer.py
@@ -1,21 +1,20 @@
 import subprocess
 import time
 import os
-import datetime
+from datetime import datetime
 
 last_trimmed = 0
 
 def trimmer():
     global last_trimmed
-    nodine_log_path = "/home/inery-node/inery.setup/master.node/blockchain/nodine.log"
+    nodine_log_path = "/home/inery-genesis/inery.setup/master.node/blockchain/nodine.log"
     log_storage = "/log_storage"
     time_created = time.time()
     oldest_file = ""
     return_nodine_size = subprocess.Popen(["sudo", "du", nodine_log_path], stdout=subprocess.PIPE)
     size_text = return_nodine_size.communicate()[0].decode()
     size_f = size_text.split("\t")[0]
-    subprocess.run(["echo", "Size of file ", str(size_f), "\n", "Last trimmed ", str(last_trimmed)])
-    # 200GB =~ 210,000,000.00 KB
+    subprocess.run(["echo", "Size of file ", str(size_f), "KB\n", "Last trimmed ", str(last_trimmed), " / ", datetime.fromtimestamp(last_trimmed).strftime("%A, %B %d, %Y %I:%M:%S")])
     if (float(size_f) > 210000000) or ((time.time() - last_trimmed) > 432000):
         for root, dirs, files in os.walk(log_storage):
             if len(files) >= 5:
@@ -28,7 +27,7 @@ def trimmer():
                     subprocess.run(["sudo", "rm", "-rf", oldest_file])
         oldest_file = ""
         subprocess.run(["sudo", "zip", "-r", log_storage + "/nodinelog" + str(time.time()) + ".zip", nodine_log_path])
-        subprocess.run(["echo", "Backuped nodine.log file ", str(datetime.datetime.now())])
+        subprocess.run(["echo", "Backuped nodine.log file ", str(datetime.now())])
         subprocess.run(["truncate", "-s", "0", nodine_log_path])
         last_trimmed = time.time()
 
